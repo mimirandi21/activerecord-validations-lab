404e30bacba37fb7852fe844e0f2fc0289d0e0d8
Randi Schreiner <mimirandi21@gmail.com>
2020-11-30 12:00:43 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { within: %w(Fiction Non-Fiction), 
+        message: "%{value} is not a valid option" }
+    validate :clickbait_title
+
+    Clickbait_matches = [
+        "Won't Believe",
+        "Secret",
+        "Top[number]",
+        "Guess"
+    ]
+
+    def clickbait_title
+        if title.present? && Clickbait_matches.none? { |p| title.match(p)}
+            errors.add(:clickbait, "needs to be more clickbait-y")
+        end
+    end
 end
